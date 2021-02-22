12cb2097334120eb0f4a51348bbc8f8f1ff48194
HandsomeSquidward <agadrian1331@gmail.com>
2021-02-22 09:06:39 -0500

    Initial Commit

---
+++ Nova/src/Nova.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Nova\Application.h"
+
+#include "Nova\EntryPoint.h"
---
+++ Nova/src/Nova/Core.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifdef NOVA_PLATFORM_WINDOWS
+	#ifdef NOVA_BUILD_DLL
+		#define NOVA_API __declspec(dllexport)
+	#else
+		#define NOVA_API __declspec(dllimport)
+	#endif // NOVA_BUILD_DLL
+
+#endif // NOVA_PLATFORM_WINDOWS
